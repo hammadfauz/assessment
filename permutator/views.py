c79ee810584cf9bedf84ff65c0df93c8174b48dc
Hammad Fauz <hammad@raspberry-pi.hammadfauz.com>
2015-07-28 11:31:36 +0400

    submission

---
+++ permutator/views.py
@@ -0,0 +1,21 @@
+# Create your views here.
+import string, itertools, json
+from django.http import HttpResponse
+from django.template import loader, Context
+
+def form(request):
+  t = loader.get_template('form.html')
+  c = Context()
+  return HttpResponse(t.render(c))
+
+def permutator(request, someString):
+  stringArray = string.split(someString)
+  if len(stringArray) > 1:
+    retval = json.dumps([' '.join(p) for p in itertools.permutations(stringArray)])
+    response = HttpResponse(retval)
+    response['Content-Type'] = 'application/json'
+    return response
+  retval = json.dumps([''.join(p) for p in itertools.permutations(someString[0:3])])
+  response = HttpResponse(retval)
+  response['Content-Type'] = 'application/json'
+  return response
